3b804d7e301175504be047709a7339c24f36151c
Ciaran Baselmans <ciaranbaselmans@gmail.com>
2014-07-08 12:15:19 +0100

    made settings.php generic

--- conf/settings.php
+++ conf/settings.php
@@ -1,9 +1,9 @@
 <?php
 	require_once 'lang/eng_UK.php';
-
+	//change settings to suit your needs
 	$dbConnection["host"]		= "localhost";
-	$dbConnection["username"]	= "root";
-	$dbConnection["password"]	= "Baselmans112";
+	$dbConnection["username"]	= "---";
+	$dbConnection["password"]	= "---";
 	$dbConnection["dbName"]		= "fussball";
 	
 	$refTimeWindowWhereClause	= " `fixture` >= DATE_SUB(NOW(), INTERVAL 1 MONTH) ";
